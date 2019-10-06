d9d879298fc1c95a12586da16909998cb6d71896
future-ruins <helliot885@gmail.com>
2019-10-07 01:12:11 +0200

    Send color data from react props to redux store

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketches/sketch';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   // Color ids are generated and color changes are recorded in local state of App
@@ -13,19 +15,26 @@ class App extends Component {
     this.randomColor = this.randomColor.bind(this);
   }
 
+  // action sets new color and dispatches that data to redux store
   randomColor() {
     this.setState({
       color: [Math.random() * 255, Math.random() * 255, Math.random() * 255],
+    });
+    store.dispatch({
+      type: 'SET_COLORS',
+      payload: this.state.color,
     });
   }
 
   // In the render function color is this.state.color
   render() {
     return (
-      <div>
-        <button onClick={this.randomColor}>Random Color</button>
-        <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
-      </div>
+      <Provider store={store}>
+        <div>
+          <button onClick={this.randomColor}>Random Color</button>
+          <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
+        </div>
+      </Provider>
     );
   }
 }
