8b0aa0b481f33627a0920c3b682ff77a37758727
future-ruins <helliot885@gmail.com>
2019-10-06 18:34:03 +0200

    Add sketch and button to change color of circle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import P5Wrapper from 'react-p5-wrapper';
+import sketch from './sketches/sketch';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Color ids are generated and color changes are recorded in local state of App
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: [Math.random() * 255, Math.random() * 255, Math.random() * 255],
+    };
+    this.randomColor = this.randomColor.bind(this);
+  }
+
+  randomColor() {
+    this.setState({
+      color: [Math.random() * 255, Math.random() * 255, Math.random() * 255],
+    });
+  }
+
+  // In the render function color is this.state.color
+  render() {
+    return (
+      <div>
+        <button onClick={this.randomColor}>Random Color</button>
+        <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
+      </div>
+    );
+  }
 }
 
 export default App;
